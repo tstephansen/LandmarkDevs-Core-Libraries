c24b4ba3ed71e977db79492284a67be6b73f92a6
Tim Stephansen <Tim.Stephansen@newmindtech.com>
2018-05-24 19:06:44 -0400

    Added IRelayCommand back to the infrastructure project.

---
+++ Src/LandmarkDevs.Core.Infrastructure/IRelayCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace LandmarkDevs.Core.Infrastructure
+{
+    /// <inheritdoc/>
+    public interface IRelayCommand : System.Windows.Input.ICommand
+    {
+        /// <summary>
+        ///     Called when the can execute value has changed.
+        /// </summary>
+        void OnCanExecuteChanged();
+
+        /// <summary>
+        ///     Raises <see cref="System.Windows.Input.ICommand.CanExecuteChanged"/> so every command
+        ///     invoker can requery to check if the command can execute.
+        /// </summary>
+        void RaiseCanExecuteChanged();
+    }
+}
